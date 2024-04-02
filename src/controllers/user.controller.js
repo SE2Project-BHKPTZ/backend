338bce354e67053c06c96343d1e5d73ff2559786
Alexander Pichler <alexander.pichler.fi@gmail.com>
2024-04-02 16:47:37 +0200

    test: added test for user route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -10,15 +10,20 @@ async function getUser(req, res, next) {
 }
 
 async function addUser(req, res, next) {
+  if (req.body.username === undefined || req.body.password === undefined) {
+    res.status(400);
+    res.json({ status: 'error', data: 'Username or password undefined' });
+    return;
+  }
   try {
-    res.json({ status: 'success', data: await userService.add(req.body.username,req.body.password) });
+    res.json({ status: 'success', data: await userService.add(req.body.username, req.body.password) });
   } catch (err) {
     console.error('Error while getting user', err.message);
     next(err);
-  }
+  }/*  */
 }
 
 module.exports = {
   getUser,
-  addUser
+  addUser,
 };
--- src/routes/user.route.test.js
+++ src/routes/user.route.test.js
@@ -1,10 +1,19 @@
 const request = require('supertest');
 const express = require('express');
+const bodyParser = require('body-parser');
 const userRouter = require('./user.route');
 
 const app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/users', userRouter);
+
+jest.mock('../services/user.service.js', () => ({
+  getAll: jest.fn().mockReturnValue([
+    { username: 'test', password: 'test' },
+  ]),
+  add: jest.fn().mockReturnValue(true),
+}));
 
 describe('Get /users', () => {
   test('should respond with a 200 status code', async () => {
@@ -20,13 +29,27 @@ describe('Get /users', () => {
 describe('Post /users/register', () => {
   test('should respond with a 200 status code', async () => {
     const response = await request(app).post('/users/register').send({
-      username:"test",
-      password:"test"
+      username: 'test',
+      password: 'test',
     });
     expect(response.statusCode).toBe(200);
   });
   test('should specify json in the content type header', async () => {
-    const response = await request(app).get('/users').send();
+    const response = await request(app).post('/users/register').send();
     expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
+  });
+  test('should have password in the request body', async () => {
+    const response = await request(app).post('/users/register').send({
+      username: undefined,
+      password: 'test',
+    });
+    expect(response.statusCode).toBe(400);
+  });
+  test('should have username in the request body', async () => {
+    const response = await request(app).post('/users/register').send({
+      username: 'test',
+      password: undefined,
+    });
+    expect(response.statusCode).toBe(400);
   });
 });
