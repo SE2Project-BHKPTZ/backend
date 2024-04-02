f6d82f25c3f7e41af5331ccc923be5173685cd66
TannerGabriel <gabrieltanner.code@gmail.com>
2024-04-02 22:30:51 +0200

    feat: Change User.save mock
    Signed-off-by: TannerGabriel <gabrieltanner.code@gmail.com>

--- src/services/user.service.test.js
+++ src/services/user.service.test.js
@@ -1,13 +1,15 @@
 const bcrypt = require('bcryptjs');
-
 const User = require('../models/user.model');
 const { add } = require('./user.service');
 
-// Mock bcrypt methods
 jest.mock('bcryptjs', () => ({
   genSalt: jest.fn(),
   hash: jest.fn(),
 }));
+
+jest.mock('../models/user.model', () => jest.fn().mockImplementation(() => ({
+  save: jest.fn(),
+})));
 
 describe('add function', () => {
   afterEach(() => {
@@ -21,40 +23,30 @@ describe('add function', () => {
     const mockedHashedPassword = 'mockedHashedPassword';
     const mockedUuid = 'mockedUuid';
 
-    // Mock bcrypt.genSalt and bcrypt.hash
     bcrypt.genSalt.mockResolvedValue(mockedSalt);
     bcrypt.hash.mockResolvedValue(mockedHashedPassword);
 
-    // Mock User.save
-    const saveMock = jest.fn().mockResolvedValueOnce({
-      username,
-      password: mockedHashedPassword,
-      uuid: mockedUuid,
-      playedGames: [],
-    });
+    User.mockImplementation(() => ({
+      save: jest.fn().mockResolvedValue({
+        username,
+        password: mockedHashedPassword,
+        uuid: mockedUuid,
+        playedGames: [],
+      }),
+    }));
 
-    // Mock the User constructor to return the mocked user instance
-    jest.spyOn(User, 'create').mockResolvedValue(saveMock);
-
-    // Call the function
     const result = await add(username, password);
 
-    // Check if bcrypt.genSalt and bcrypt.hash were called with the correct arguments
     expect(bcrypt.genSalt).toHaveBeenCalledWith(10);
     expect(bcrypt.hash).toHaveBeenCalledWith(password, mockedSalt);
 
-    // Check if User constructor was called with the correct arguments
     expect(User).toHaveBeenCalledWith({
       username,
       password: mockedHashedPassword,
-      uuid: mockedUuid,
+      uuid: expect.any(String),
       playedGames: [],
     });
 
-    // Check if the user was saved
-    expect(saveMock).toHaveBeenCalled();
-
-    // Check if the result is correct
     expect(result).toEqual({
       username,
       password: mockedHashedPassword,
