f9fc0490b8241ed7f7a1b8703d2e0a733f207cd6
TannerGabriel <gabrieltanner.code@gmail.com>
2024-04-02 09:14:36 +0200

    feat: Fix linting issues
    Signed-off-by: TannerGabriel <gabrieltanner.code@gmail.com>

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,12 @@
 async function getUser(req, res, next) {
-    try {
-        res.json({status: "success", data: {userId: "demo"}});
-    } catch (err) {
-        console.error(`Error while getting user`, err.message);
-        next(err);
-    }
+  try {
+    res.json({ status: 'success', data: { userId: 'demo' } });
+  } catch (err) {
+    console.error('Error while getting user', err.message);
+    next(err);
+  }
 }
 
 module.exports = {
-    getUser
+  getUser,
 };
