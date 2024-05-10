be8337529264dc2199e74bb4153852f7500edba6
Alexander Pichler <alexander.pichler.fi@gmail.com>
2024-05-10 12:22:33 +0200

    refactor: removed dummy data

--- src/services/gamestate.service.js
+++ src/services/gamestate.service.js
@@ -1,37 +1,4 @@
-const games = {
-  lobbyid: {
-    players: {
-      '29e315f2-beb1-403b-a3e1-1a452cf76e5b': {
-        cards: [
-          'Diamonds-0', 'Hearts-3', 'Clubs-10', 'Spades-6',
-        ],
-        points: 100,
-      },
-      '3434a03a-c9f0-4bd8-9925-91bf61d36e76': {},
-      '9d93ce7a-c893-4f17-ad88-bf5f0ae09493': {},
-    },
-    rounds: [
-      {
-        predictions: {
-          'f3d4e005-2960-4513-ac7c-ee60e1e88209': 1,
-          '2b921e33-74e6-4e6a-a46a-c6f5f125ac7b': 5,
-          'b2291922-d9c1-4c1b-a583-bbe707f9c413': 5,
-        },
-        subrounds: [
-          {
-            cardsPlayed: [
-              { player: 'f3d4e005-2960-4513-ac7c-ee60e1e88209', card: 'Diamonds-0' },
-              { player: 'f3d4e005-2960-4513-ac7c-ee60e1e88209', card: 'Diamonds-1' },
-              { player: 'f3d4e005-2960-4513-ac7c-ee60e1e88209', card: 'Diamonds-2' },
-            ],
-            stichPlayer: '9aee6f84-89f2-44d5-9170-1e9d756c1fc2',
-          },
-        ],
-      },
-    ],
-    nextPlayer: 'fe442c0d-f2fd-4dd1-a54f-ebad839c8a35',
-  },
-};
+const games = {};
 
 function createPlayerFields(players) {
   const playerFields = {};
