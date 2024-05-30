a9347e7480aafb545819f2ca77aaa8d98f53f228
Alexander Pichler <alexander.pichler.fi@gmail.com>
2024-05-30 10:29:56 +0200

    added custom round limit

--- src/services/game.service.js
+++ src/services/game.service.js
@@ -1,21 +1,8 @@
 const Deck = require('../utils/deck.model');
 
-const isLastRound = (round, playerCount) => {
-  switch (playerCount) {
-    case 3:
-      return round === 20;
-    case 4:
-      return round === 15;
-    case 5:
-      return round === 12;
-    case 6:
-      return round === 10;
-    default:
-      return false;
-  }
-};
+const isLastRound = (round, maxRounds) => round === maxRounds;
 
-exports.startRound = (round, playerCount) => {
+exports.startRound = (round, playerCount, maxRounds) => {
   if (round < 1 || round > 20) { throw new Error('the count of rounds must be from 1 to 20'); }
 
   if (playerCount < 3 || playerCount > 6) { throw new Error('the player count must be from 3 to 6'); }
@@ -24,7 +11,7 @@ exports.startRound = (round, playerCount) => {
   deck.shuffle();
   return {
     hands: deck.deal(round, playerCount),
-    trump: isLastRound(round, playerCount) ? null : deck.drawCard(),
+    trump: isLastRound(round, maxRounds) ? null : deck.drawCard(),
   };
 };
 
